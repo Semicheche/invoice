86159333df688774fb40ea13ea91998f16d3b4ca
Luciano Semicheche <lucianosemicheche@gmail.com>
2019-04-28 21:08:48 -0300

    [add] test unit and crete method save to binary file

--- lib/invoice.ex
+++ lib/invoice.ex
@@ -6,18 +6,41 @@ defmodule Invoice do
   @doc """
     Receive a `invoice` and return a array list
       ## Example:
-        iex> Invoice.make_invoice(["Agua", "Telefone", "Luz"])
-          result => ["Agua", "Telefone", "Luz"]
+      iex> Invoice.make_invoice(["Agua", "Telefone", "Luz"])
+      ["Agua", "Telefone", "Luz"]
   """
-  def make_invoice(invoices) do
-    invoices
+  def make_invoice(invoices,  salaries) do
+    #make two for but this form is euqual at method flatten
+    for due <- salaries, invoice <- invoices do
+        " Invoice: #{invoice} due date: #{due} "     # <- Template String
+    end
   end
 
+  def save(file_name, invoices) do
+      File.write(file_name, :erlang.term_to_binary(invoices))
+  end
+
+  def invoice_to_be_paid(invoices, qty) do
+    Enum.split(invoices, qty)
+  end
+  @doc """
+    Receive a `invoices list` and return a array list ordened
+      ## Example:
+      iex> Invoice.ordened_invoice(Invoice.make_invoice(["Agua", "Telefone", "Luz"]))
+      ["Agua", "Luz", "Telefone"]
+  """
   def ordened_invoice(invoices) do
     Enum.sort(invoices)
   end
 
-  def pass do
-
+  @doc """
+    Receive a `invoices list` and  a `invoice` a return true if exist or False if not exist
+      ## Example:
+      iex> Invoice.invoice_exist?(Invoice.make_invoice(["Agua", "Telefone", "Luz"]), "Luz")
+      true
+  """
+  def invoice_exist?(invoices, invoice) do
+    Enum.member?(invoices, invoice)
   end
+
 end
--- test/invoice_test.exs
+++ test/invoice_test.exs
@@ -2,7 +2,16 @@ defmodule InvoiceTest do
   use ExUnit.Case
   doctest Invoice
 
-  test "greets the world" do
-    assert Invoice.hello() == :world
+  test "must be make invoices list" do
+    invoices = Invoice.make_invoice(["Agua", "Luz", "Telefone"])
+    
+    assert invoices == ["Agua", "Luz", "Telefone"]
   end
+
+  test "must be return list invoices ordened" do
+    invoices = Invoice.make_invoice(["Agua", "Telefone", "Luz"])
+
+    refute Invoice.ordened_invoice(invoices) == ["Agua", "Telefone", "Luz"]
+  end
+
 end
